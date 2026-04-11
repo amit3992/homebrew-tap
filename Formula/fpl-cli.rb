dcbd6351725998ea2a57ff685a7bed7f63595497
Amit Kulkarni <amit3992@gatech.edu>
2026-04-10 21:58:59 -0700

    Update fpl-cli to v0.2.0

--- Formula/fpl-cli.rb
+++ Formula/fpl-cli.rb
@@ -1,8 +1,8 @@
 class FplCli < Formula
   desc "Command-line tool for Fantasy Premier League"
   homepage "https://github.com/amit3992/fpl-cli"
-  url "https://github.com/amit3992/fpl-cli/releases/download/v0.1.0/amit3992-fpl-cli-0.1.0.tgz"
-  sha256 "8885bf87589696c52c868af8222af8ade7cf01062e12e60996bb5f63b8b8e96d"
+  url "https://github.com/amit3992/fpl-cli/releases/download/v0.2.0/amit3992-fpl-cli-0.2.0.tgz"
+  sha256 "1b613abef631d838a76e6b0375db28285bbce1d4935512de914ba9d50a6e1833"
   license "MIT"
 
   depends_on "node"
